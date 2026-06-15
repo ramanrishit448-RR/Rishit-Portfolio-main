137609ef8cd4436fd94a85d0cf2a6f3d3f309de8
ramanrishit448-RR <ramanrishit448@gmail.com>
2026-06-15 23:03:17 +0530

    Update Navbar sections to include 'skills' instead of 'services'

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -106,7 +106,7 @@ const Navbar = () => {
         className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2"
       >
         <div className="flex flex-col text-5xl gap-y-2 md:text-6xl lg:text-8xl">
-          {["home", "services", "about", "work", "contact"].map(
+          {["home", "skills", "about", "work", "contact"].map(
             (section, index) => (
               <div key={index} ref={(el) => (linksRef.current[index] = el)}>
                 <Link
